3f2bbc9d80bc4897ab8fc09c5a0f90c443a27c30
Courtney Dyson <courtneydyson87@gmail.com>
2019-08-16 11:17:43 -0400

    Crushed it

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,23 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def show
+    @power = Power.find(params[:id])
+  end
+
+  def new
+    @power = Power.new
+  end
+
+  def create
+    
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
 end
