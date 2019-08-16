35aef60f644eed2ec6da7df3e2d8e4b408d9374d
John Odom <john.osborne.odom@gmail.com>
2019-08-16 11:10:08 -0400

    Crushed It

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,18 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+  def new
+  @heroine = Heroine.new
+  end
+  def create
+    @heroine = Heroine.new(params.require(:heroine).permit(:name, :super_name, :power_id))
+    if @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      render :new
+    end
+  end
 end
