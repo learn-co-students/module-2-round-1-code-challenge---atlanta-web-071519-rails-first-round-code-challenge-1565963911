d52e14a5b653a2185c9b26a8ed0bbe41f00c81d5
Jourdan Hamilton <jourdan.hamilton1@gmail.com>
2019-08-16 11:17:25 -0400

    crushed it

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,20 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end 
+
+  def new
+    @heroine = Heroine.new
+  end 
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+	
+  
+  end 
+  
+
 end
